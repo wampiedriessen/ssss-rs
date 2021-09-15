50ecba0af3175c9619c7e51352223904e37ee001
Wampie Driessen <wampiedriessen@users.noreply.github.com>
2021-09-15 19:35:57 +0200

    Remove superflous SsssData struct

--- src/lib.rs
+++ src/lib.rs
@@ -7,29 +7,13 @@ use std::str;
 #[derive(Debug)]
 struct SsssShard {
     shard_number: u8,
-    data: SsssData,
-}
-
-#[derive(Debug)]
-struct SsssData {
     data: Vec<u8>,
-}
-
-impl SsssData {
-    fn new(data: Vec<u8>) -> SsssData {
-        SsssData { data }
-    }
 }
 
 impl fmt::Display for SsssShard {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}-{}", self.shard_number, self.data)
-    }
-}
-
-impl fmt::Display for SsssData {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", base64_encode(self.data.as_slice()))
+        let data_formatted = base64_encode(self.data.as_slice());
+        write!(f, "{}-{}", self.shard_number, data_formatted)
     }
 }
 
@@ -40,17 +24,7 @@ impl str::FromStr for SsssShard {
         let split: Vec<&str> = s.split('-').collect();
         Ok(SsssShard {
             shard_number: split[0].parse().unwrap(),
-            data: split[1].parse().unwrap(),
-        })
-    }
-}
-
-impl str::FromStr for SsssData {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(SsssData {
-            data: base64_decode(s),
+            data: base64_decode(split[1]),
         })
     }
 }
@@ -59,11 +33,11 @@ impl str::FromStr for SsssData {
 mod tests {
     use super::*;
 
-    static FORMATTED_STRING: &str = "13-QUJDQQ==";
+    const FORMATTED_STRING: &str = "13-QUJDQQ==";
     fn example_shard() -> SsssShard {
         SsssShard {
             shard_number: 13,
-            data: SsssData::new(vec![65, 66, 67, 65]),
+            data: vec![65, 66, 67, 65],
         }
     }
 
@@ -80,6 +54,6 @@ mod tests {
 
         let s = example_shard();
         assert_eq!(s.shard_number, shard.shard_number);
-        assert_eq!(s.data.data, shard.data.data);
+        assert_eq!(s.data, shard.data);
     }
 }
