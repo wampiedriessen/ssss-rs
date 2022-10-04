72f25c65c8b8f02189fd74eb974cae91c2e9a15f
Wampie Driessen <wampiedriessen@users.noreply.github.com>
2022-10-04 16:28:25 +0200

    Abstracise i/o with defaults to stdin/stdout

---
+++ src/inputoutput.rs
@@ -0,0 +1,30 @@
+use std::{path::PathBuf, io::{Write, Read}};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub(crate) struct InputOutput {
+    /// Input file, stdin if not present
+    #[structopt(parse(from_os_str), short, long)]
+    input: Option<PathBuf>,
+
+    /// Output file, stdout if not present
+    #[structopt(parse(from_os_str), short, long)]
+    output: Option<PathBuf>,
+}
+
+impl InputOutput {
+
+    pub(crate) fn get_output(&self) -> Result<Box<dyn Write>, String> {
+        if let Some(file) = &self.output {
+            Ok(Box::new(std::fs::File::create(file).map_err::<String, _>(|_| "Could not open output file!".into())?))
+        }
+        else { Ok(Box::new(std::io::stdout())) }
+    }
+    
+    pub(crate) fn get_input(&self) -> Result<Box<dyn Read>, String> {
+        if let Some(file) = &self.input {
+            Ok(Box::new(std::fs::File::open(file).map_err::<String, _>(|_| "Could not open input file!".into())?))
+        }
+        else { Ok(Box::new(std::io::stdin())) }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::io::{Read, Write};
-use std::path::PathBuf;
+use inputoutput::InputOutput;
 use structopt::StructOpt;
 use rs_ssss::shamir::ShamirStd;
 use rs_ssss::shard::SsssShard;
 
+mod inputoutput;
 mod encoding;
 mod math;
 pub mod shamir;
@@ -26,9 +26,9 @@ enum Action {
 #[derive(Debug, StructOpt)]
 #[structopt(name = "ssss-rs", about = "Shards a secret, and merges the pieces back together")]
 struct SsssRsOpt {
-    /// Output file, stdout if not present
-    #[structopt(parse(from_os_str), short, long)]
-    output: Option<PathBuf>,
+
+    #[structopt(flatten)]
+    io: InputOutput,
 
     #[structopt(subcommand, name = "action", help = "Want to create, or combine shards?")]
     action: Action,
@@ -38,8 +38,8 @@ fn main() -> Result<(), String> {
     let opt = SsssRsOpt::from_args();
 
     let result = match opt.action {
-        Action::Shard { threshold, number_of_shards } => create_shards(threshold, number_of_shards, opt.output),
-        Action::Merge => merge_shards(opt.output),
+        Action::Shard { threshold, number_of_shards } => create_shards(threshold, number_of_shards, &opt.io),
+        Action::Merge => merge_shards(&opt.io),
     };
 
     if let Err(message) = result {
@@ -50,13 +50,13 @@ fn main() -> Result<(), String> {
     Ok(())
 }
 
-fn merge_shards(output: Option<PathBuf>) -> Result<(), String> {
-    let input = std::io::stdin();
+fn merge_shards(io: &InputOutput) -> Result<(), String> {
     let mut shards = Vec::<SsssShard>::new();
+    let mut input = io.get_input()?;
 
     let mut input_buffer = String::new();
     let mut line = 1;
-    while input.read_line(&mut input_buffer).map_err::<String, _>(|_| "Could not read input!".into())? != 0 {
+    while input.read_to_string(&mut input_buffer).map_err::<String, _>(|_| "Could not read input!".into())? != 0 {
         shards.push(input_buffer.trim().parse().map_err(|x| format!("{} on line {}", x, line))?);
         input_buffer.clear();
         line += 1;
@@ -64,35 +64,25 @@ fn merge_shards(output: Option<PathBuf>) -> Result<(), String> {
 
     let secret = ShamirStd::merge_shards(&shards);
 
-    let mut out = get_output(output)?;
+    let mut out = io.get_output()?;
 
     writeln!(out, "").map_err::<String, _>(|_| "Could not write output!".into())?;
 
     out.write_all(secret.as_slice()).map_err(|_| "Could not write output!".into())
 }
 
-fn create_shards(thresh: u8, num: u8, output: Option<PathBuf>) -> Result<(), String> {
-    let mut input = std::io::stdin();
-    let mut input_buffer = Vec::new();
+fn create_shards(thresh: u8, num: u8, io: &InputOutput) -> Result<(), String> {
+let mut input_buffer = Vec::new();
 
-    input.read_to_end(&mut input_buffer).map_err::<String, _>(|_| "Could not read input!".into())?;
+    io.get_input()?.read_to_end(&mut input_buffer).map_err::<String, _>(|_| "Could not read input!".into())?;
 
     let shamir = ShamirStd::new(thresh, num);
-
     let shards = shamir.create_shards(input_buffer.as_slice());
-
-    let mut out = get_output(output)?;
+    let mut out = io.get_output()?;
 
     for shard in shards {
         writeln!(out, "{}", shard).map_err::<String, _>(|_| "Could not write output!".into())?;
     }
 
     Ok(())
-}
-
-fn get_output(output: Option<PathBuf>) -> Result<Box<dyn Write>, String> {
-    if let Some(file) = output {
-        Ok(Box::new(std::fs::File::create(file).map_err::<String, _>(|_| "Could not open file!".into())?))
-    }
-    else { Ok(Box::new(std::io::stdout())) }
 }
